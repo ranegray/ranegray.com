5907a53f1fc7898b2a8cf60b9a451a8a1e973bab
Rane Gray <ranegray830@gmail.com>
2025-04-28 13:38:37 -0600

    update socials and nav links

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 
 const NavLink = ({ href, children }) => (
-  <a
+  <Link
     href={href}
     className="text-zinc-600 hover:text-orange-600 transition-colors px-3 py-2"
   >
     {children}
-  </a>
+  </Link>
 );
 
 const ProjectCard = ({ title, description, technologies }) => (
@@ -35,17 +35,12 @@ export default function Home() {
       <header className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 pt-6">
         <nav className="flex items-center justify-between">
           <h1 className="text-3xl font-bold">rane<span className="text-orange-500 font-black">.</span></h1>
-          <ul className="flex gap-4">
-            <li>
-              <Link href="/">home</Link>
-            </li>
-            <li>
-              <Link href="/about">about</Link>
-            </li>
-            <li>
-              <Link href="/contact">contact</Link>
-            </li>
-          </ul>
+          <div className="hidden md:flex items-center">
+              <NavLink href="/">Home</NavLink>
+              <NavLink href="/projects">Projects</NavLink>
+              <NavLink href="/blog">Blog</NavLink>
+              <NavLink href="/about">About</NavLink>
+            </div>
         </nav>
       </header>
       <main className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
@@ -146,19 +141,19 @@ export default function Home() {
             <p className="text-zinc-500">© 2025 Rane Gray</p>
           </div>
           <div className="flex space-x-6">
-            <a href="#" className="text-zinc-500 hover:text-orange-600">
-              <span className="sr-only">Twitter</span>
+            <a href="https://x.com/ranegray_" className="text-zinc-500 hover:text-orange-600">
+              <span className="sr-only">X.com</span>
               <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+                <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
               </svg>
             </a>
-            <a href="#" className="text-zinc-500 hover:text-orange-600">
+            <a href="https://github.com/ranegray" className="text-zinc-500 hover:text-orange-600">
               <span className="sr-only">GitHub</span>
               <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
                 <path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clipRule="evenodd" />
               </svg>
             </a>
-            <a href="#" className="text-zinc-500 hover:text-orange-600">
+            <a href="https://www.linkedin.com/in/ranegray" className="text-zinc-500 hover:text-orange-600">
               <span className="sr-only">LinkedIn</span>
               <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
